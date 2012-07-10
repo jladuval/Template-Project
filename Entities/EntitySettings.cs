5961a33d3fc6e164470127dea250a1c47636f9ef
JDuval <jladuval@gmail.com>
2012-07-10 21:48:25 +1200

    Added configurations and Settings for entities

---
+++ Entities/EntitySettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Entities
+{
+    public struct EntitySettings
+    {
+        public const int FirstNameLength = 50;
+
+        public const int LastNameLength = 50;
+
+        public const int EmailLength = 50;
+
+        public const int PasswordLength = 128;
+
+        public const int ConfirmationTokenLength = 128;
+
+        public const int RoleNameLength = 50;
+
+        public const int RoleDescriptionLength = 100;
+    }
+}
